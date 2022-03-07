c968570d61cc50fc3f95f83f37a43e7c2691bf44
mwarddev <martinward1981@googlemail.com>
2022-03-07 01:30:19 +0000

    Replace navbar items with icons

--- exhibition/urls.py
+++ exhibition/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.CategorySelect.as_view(), name='home'),
     path('posts/<str:artform>/', views.SelectedCategory.as_view(), name='artform'),
-    path('posts/<str:slug>', views.FullPostView.as_view(), name='full_post'),
+    path('<str:slug>/', views.FullPostView.as_view(), name='full_post'),
 ]
