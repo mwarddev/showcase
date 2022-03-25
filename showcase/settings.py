3393d32b0eed80adb0eb69562784507ca8e55180
mwarddev <martinward1981@googlemail.com>
2022-03-25 19:08:33 +0000

    Add cloudinary setting in settings.py

--- showcase/settings.py
+++ showcase/settings.py
@@ -153,6 +153,7 @@ STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+CLOUDINARY_STORAGE = { 'CLOUDINARY_URL': os.environ.get('CLOUDINARY_URL')}
 
 
 MEDIA_URL = '/media/'
